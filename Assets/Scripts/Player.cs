9024f1136aeefa15bb6b2a990a142f3dc6ab38cd
DaNk567 <55024284+venomcod@users.noreply.github.com>
2022-11-25 22:50:38 +0500

    доделать вращение!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,31 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Tooltip("м/c")][SerializeField] float xSpeed = 4f;
-    [Tooltip("м/c")][SerializeField] float ySpeed = 4f;
+    [Tooltip("м/c")][SerializeField] float Speed = 4f;
     [Tooltip("Ограничение по X")][SerializeField] float XClamp = 17f;
     [Tooltip("Ограниченик по Y")][SerializeField] float YClamp = 17f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        RotatePlayer();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+
+    void MovePlayer()
+    {
         float xMove = Input.GetAxis("Horizontal");
-        float xOffset = xMove * xSpeed * Time.deltaTime;
+        float xOffset = xMove * Speed * Time.deltaTime;
         float newXPos = transform.localPosition.x + xOffset;
         float xClampPos = Mathf.Clamp(newXPos, -XClamp, XClamp);
 
         float yMove = Input.GetAxis("Vertical");
-        float yOffset = yMove * ySpeed * Time.deltaTime;
+        float yOffset = yMove * Speed * Time.deltaTime;
         float newYPos = transform.localPosition.y + yOffset;
         float yClampPos = Mathf.Clamp(newYPos, -YClamp, YClamp);
 
         transform.localPosition = new Vector3(xClampPos, yClampPos, transform.localPosition.z);
     }
+
+    void RotatePlayer()
+    {
+        transform.localRotation = Quaternion.Euler(-30, 30, 0);
+    }
+
 }
 
