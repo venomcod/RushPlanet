6f7465fd7bbf0a045c6b6e699da6a5beb2e29758
DaNk567 <55024284+venomcod@users.noreply.github.com>
2022-11-25 17:00:02 +0500

    ДОДЕЛАЛ УПРАВЛЕНИЕ

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-
-    [SerializeField] InputAction controlMove;
-
+    [Tooltip("м/c")][SerializeField] float xSpeed = 4f;
+    [Tooltip("м/c")][SerializeField] float ySpeed = 4f;
+    [Tooltip("Ограничение по X")][SerializeField] float XClamp = 17f;
+    [Tooltip("Ограниченик по Y")][SerializeField] float YClamp = 17f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,22 +15,20 @@ public class Player : MonoBehaviour
         
     }
 
-    void OnEnable()
-    {
-        controlMove.Enable();
-    }
-
-    void OnDisable()
-    {
-        controlMove.Disable();
-    }
-
     // Update is called once per frame
     void Update()
     {
-        float horizontalMove = controlMove.ReadValue<Vector2>().x;
-        float verticalMove = controlMove.ReadValue<Vector2>().y;
-        Debug.Log(horizontalMove);
-        Debug.Log(verticalMove);
+        float xMove = Input.GetAxis("Horizontal");
+        float xOffset = xMove * xSpeed * Time.deltaTime;
+        float newXPos = transform.localPosition.x + xOffset;
+        float xClampPos = Mathf.Clamp(newXPos, -XClamp, XClamp);
+
+        float yMove = Input.GetAxis("Vertical");
+        float yOffset = yMove * ySpeed * Time.deltaTime;
+        float newYPos = transform.localPosition.y + yOffset;
+        float yClampPos = Mathf.Clamp(newYPos, -YClamp, YClamp);
+
+        transform.localPosition = new Vector3(xClampPos, yClampPos, transform.localPosition.z);
     }
 }
+
