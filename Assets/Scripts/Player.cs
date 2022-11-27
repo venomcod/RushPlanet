753710b5ec470239648c88f73dbdaacc50f49d9d
DaNk567 <55024284+venomcod@users.noreply.github.com>
2022-11-27 14:09:39 +0500

    Доделал вращение

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,28 +7,37 @@ public class Player : MonoBehaviour
     [Tooltip("м/c")][SerializeField] float Speed = 4f;
     [Tooltip("Ограничение по X")][SerializeField] float XClamp = 17f;
     [Tooltip("Ограниченик по Y")][SerializeField] float YClamp = 17f;
+    [SerializeField] float xRotFacrtor = -0.7f;
+    [SerializeField] float yRotFacrtor = -0.7f;
+    [SerializeField] float zRotFacrtor = 4f;
+    [SerializeField] float xMoveRot = 0f;
+    [SerializeField] float yMoveRot = 0f;
+    [SerializeField] float zMoveRot = 0f;
+
+    float xMove, yMove;
 
     // Start is called before the first frame update
     void Start()
     {
-        RotatePlayer();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         MovePlayer();
+        RotatePlayer();
     }
 
 
     void MovePlayer()
     {
-        float xMove = Input.GetAxis("Horizontal");
+        xMove = Input.GetAxis("Horizontal");
         float xOffset = xMove * Speed * Time.deltaTime;
         float newXPos = transform.localPosition.x + xOffset;
         float xClampPos = Mathf.Clamp(newXPos, -XClamp, XClamp);
 
-        float yMove = Input.GetAxis("Vertical");
+        yMove = Input.GetAxis("Vertical");
         float yOffset = yMove * Speed * Time.deltaTime;
         float newYPos = transform.localPosition.y + yOffset;
         float yClampPos = Mathf.Clamp(newYPos, -YClamp, YClamp);
@@ -38,7 +47,10 @@ public class Player : MonoBehaviour
 
     void RotatePlayer()
     {
-        transform.localRotation = Quaternion.Euler(-30, 30, 0);
+        float xRot = transform.localPosition.y * xRotFacrtor + yMove * xMoveRot;
+        float yRot = transform.localPosition.x * yRotFacrtor + xMove * yMoveRot;
+        float zRot = xMove * zMoveRot;
+        transform.localRotation = Quaternion.Euler(xRot, yRot, zRot);
     }
 
 }
